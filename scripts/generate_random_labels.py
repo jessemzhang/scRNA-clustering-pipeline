404a4c9e47821f4f6719877752bfca36c0a665b6
Jesse Min Zhang <jessez@stanford.edu>
2015-07-21 11:14:53 -0700

    updated generate_random_labels, a bit of reorganizing

--- scripts/generate_random_labels.py
+++ scripts/generate_random_labels.py
@@ -6,12 +6,13 @@ import numpy as np
 #num_entries = int(sys.stdin.read())
 
 num_entries = int(sys.argv[1])
+ratio = int(sys.argv[2])
 x = np.random.uniform(0,1,(num_entries,1))
 
-f = open('labels_random.txt','w')
+f = open('../labels/labels_random.txt','w')
 i = 0
 for entry in x:
-    if x[i] > 0.5:
+    if x[i] > ratio:
         print >> f, '1'
     else:
         print >> f, '0'
